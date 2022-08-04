6fc711d176191bc9ee0664fea7788d2b759d6ca5
MM0819 <MM0819@protonmail.com>
2022-08-04 19:10:29 +0100

    fix sleep utils test

--- src/test/java/com/vega/protocol/utils/SleepUtilsTest.java
+++ src/test/java/com/vega/protocol/utils/SleepUtilsTest.java
@@ -2,9 +2,6 @@ package com.vega.protocol.utils;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 public class SleepUtilsTest {
 
     private final SleepUtils sleepUtils = new SleepUtils();
@@ -12,8 +9,9 @@ public class SleepUtilsTest {
     @Test
     public void testSleepWithInterrupt() {
         Runnable task = () -> sleepUtils.sleep(5000L);
-        ExecutorService executorService = Executors.newCachedThreadPool();
-        executorService.submit(task);
-        executorService.shutdown();
+        final Thread thread = new Thread(task);
+        thread.start();
+        thread.interrupt();
+        sleepUtils.sleep(1000L);
     }
 }
