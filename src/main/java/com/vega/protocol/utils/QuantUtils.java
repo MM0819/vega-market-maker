111146037ba2e250c0cdc6e4a8455b0bfe6af21e
MM0819 <MM0819@protonmail.com>
2022-10-30 13:22:24 +0000

    quant utils for prob of trading

---
+++ src/main/java/com/vega/protocol/utils/QuantUtils.java
@@ -0,0 +1,41 @@
+package com.vega.protocol.utils;
+
+import com.vega.protocol.constant.MarketSide;
+import org.apache.commons.math3.special.Erf;
+import org.springframework.stereotype.Component;
+
+@Component
+public class QuantUtils {
+    public double getProbabilityOfTrading(
+            final double mu,
+            final double sigma,
+            final double s,
+            final double tau,
+            final double lowerBound,
+            final double upperBound,
+            final double price,
+            final MarketSide side
+    ) {
+        double stdev = sigma * Math.sqrt(tau);
+        double m = Math.log(s) + (mu-0.5*sigma*sigma)*tau;
+        double min = 0.0;
+        double max = 1.0;
+        double z = 1.0;
+        if(lowerBound > 0.0 && upperBound > 0.0) {
+            if(price < lowerBound || price > upperBound) {
+                return 0;
+            }
+            min = cdf(m, stdev, lowerBound);
+            max = cdf(m, stdev, upperBound);
+            z = max - min;
+        }
+        if(side.equals(MarketSide.BUY)) {
+            return (cdf(m, stdev, price) - min) / z;
+        }
+        return (max - cdf(m, stdev, price)) / z;
+    }
+
+    private double cdf(double m, double stdev, double x) {
+        return 0.5 * Erf.erfc(-(Math.log(x)-m)/(Math.sqrt(2.0)*stdev));
+    }
+}
---
+++ src/test/java/com/vega/protocol/utils/QuantUtilsTest.java
@@ -0,0 +1,30 @@
+package com.vega.protocol.utils;
+
+import com.vega.protocol.constant.MarketSide;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class QuantUtilsTest {
+
+    private QuantUtils quantUtils;
+
+    @BeforeEach
+    public void setup() {
+        quantUtils = new QuantUtils();
+    }
+
+    @Test
+    public void testGetProbabilityOfTrading() {
+        double mu = 0;
+        double tau = 1.0 / 365.25 / 24.0;
+        double s = 100;
+        double sigma = 1.2;
+        double lowerBound = 95.0;
+        double upperBound = 100.0;
+        MarketSide side = MarketSide.BUY;
+        double price = 99.9;
+        double result = quantUtils.getProbabilityOfTrading(mu, sigma, s, tau, lowerBound, upperBound, price, side);
+        Assertions.assertEquals(result, 0.0);
+    }
+}
